7bebd698edd230e228cbc4c46e0f9d2da0255c87
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2024-05-23 17:44:15 +0600

    ok

--- frontend/pages/AddQuestion/AddQuestion.jsx
+++ frontend/pages/AddQuestion/AddQuestion.jsx
@@ -1,7 +1,5 @@
 import { useContext, useEffect, useRef, useState } from "react";
-import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import { useContext, useEffect, useRef, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { Button } from "@/components/ui/button";
